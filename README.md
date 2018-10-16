d7ec3893b61f5beb26da5de2f86cbff26f5c5237
Mariusz Kolodziej <mariuszkolodziej@macmacs-iMac.local>
2018-10-16 09:43:38 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Search Flickr App
+
+This is a simple app that use free Flickr API to search images by tags and display on the page.
+
+Clone repo and your project should look like this:
+
+```
+my-app/
+  README.md
+  node_modules/
+  package.json
+  public/
+    index.html
+    favicon.ico
+  src/
+    App.css
+    App.js
+    App.test.js
+    index.css
+    index.js
+    logo.svg
+```
+
+Go to: [https://www.flickr.com/services/api/](https://www.flickr.com/services/api/) to register your API key. Update App.js - line 8 with your API key.
+
+```sh
+apiKey: "c2ec20695", // update with your API key
+```
+
+In the project directory, you can run:
+
+```sh
+npm start
+```
+
+This will run the app in the development mode.
+Go to: [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits. You will also see any lint errors in the console.
+
+App will display 20 photos for "rock concert" tag as a default state. You can use input box to search for different tags. Use "Load more phootos" button at the bottom of the page to load next 20 photos.
+
+### Todos
+
+- Move search box to new component
+- Fix validation for search box after editing tags
+- update header with react-helmet (Meta Tag Management)
+- add real name from Flickr
+
+## License
+
+MIT
---
+++ src/App.js
@@ -0,0 +1,137 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      apiKey: "6f635fc1faad83feb9c2eb0489c20695",
+      pictures: [],
+      textInput: "rock concert",
+      itemsPerPage: 20
+    };
+  }
+
+  componentDidMount() {
+    this.reloadImages();
+  }
+
+  // reload images
+  reloadImages = () => {
+    fetch(
+      "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
+        this.state.apiKey +
+        "&tags=" +
+        this.state.textInput +
+        "&per_page=" +
+        this.state.itemsPerPage +
+        "&page=1&format=json&nojsoncallback=1&flickr.photos.getSizes=1&tag_mode=all"
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(
+        function(j) {
+          // alert(JSON.stringify(j));
+          let picArray = j.photos.photo.map(pic => {
+            var srcPath =
+              "https://farm" +
+              pic.farm +
+              ".staticflickr.com/" +
+              pic.server +
+              "/" +
+              pic.id +
+              "_" +
+              pic.secret +
+              ".jpg";
+
+            var picTitle = pic.title;
+            var username = pic.owner;
+            var keyId = pic.id;
+            var photoUrl = "http://flickr.com/photo.gne?id=" + keyId;
+
+            return (
+              <div key={keyId}>
+                <div className="pictureContiner">
+                  <div className="pictureCont">
+                    {picTitle} by {username}{" "}
+                    <a
+                      target="_blank"
+                      href={photoUrl}
+                      rel="noopener noreferrer"
+                    >
+                      See on Flickr
+                    </a>
+                  </div>
+                  <img alt={picTitle} src={srcPath} />
+                </div>
+              </div>
+            );
+          });
+          this.setState({ pictures: picArray });
+        }.bind(this)
+      );
+  };
+
+  // this will handle search box input change
+  HandleChange = e => {
+    this.setState({ textInput: e.target.value });
+  };
+
+  //this will handle load more click on button and add 20 more photos
+  HandlerLoadMore = () => {
+    var currentPhotosPerPage = this.state.itemsPerPage;
+    currentPhotosPerPage += 20;
+    this.setState({ itemsPerPage: currentPhotosPerPage }, () => {
+      console.log(this.state.itemsPerPage);
+      this.reloadImages();
+    });
+  };
+
+  // delay to wait 1s after user stops typing
+  Delay = (function() {
+    var timer = 0;
+    return function(callback, ms) {
+      clearTimeout(timer);
+      timer = setTimeout(callback, ms);
+    };
+  })();
+
+  // rednder...
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} key={Math.random()} className="App-logo" alt="logo" />
+          <img
+            src="//icons.iconarchive.com/icons/custom-icon-design/pretty-social-media/256/flickr-2-icon.png"
+            alt="logo"
+            className="flickrLogo"
+          />
+        </header>
+        <div id="search">
+          <input
+            className="textInput"
+            onChange={this.HandleChange}
+            onKeyUp={() =>
+              this.Delay(
+                function() {
+                  this.reloadImages();
+                }.bind(this),
+                1000
+              )
+            }
+            placeholder="What are you looking for ?"
+          />
+        </div>
+        <div id="photos" className="App-intro">
+          {this.state.pictures}
+        </div>
+        <button onClick={this.HandlerLoadMore}>Load more photos</button>
+      </div>
+    );
+  }
+}
+
+export default App;
